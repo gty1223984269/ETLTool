b4377b9b3c9590cf1b1202c43eb4f785893c71ac
jack.guo <ext.jack.guo@ef.com>
2018-09-25 18:33:37 +0800

    init

---
+++ ETLTool/ETLTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ETLTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
